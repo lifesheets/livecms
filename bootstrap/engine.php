b9b5a31d5184d4cf7dcbe1c37bf734b68be582eb
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-28 11:17:31 +0200

    Автоматичне завантаження класів та функцій

--- bootstrap/engine.php
+++ bootstrap/engine.php
@@ -3,3 +3,6 @@
 # Константи та псевдо змінних та функцій
 require_once(ROOT_DIR . '/bootstrap/system/redefinition.php');
 
+# Автоматичне завантаження класів та функцій
+require_once ROOT_DIR . '/bootstrap/system/autoload.php';
+
