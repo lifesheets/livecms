f0362bb00f9ae8486bcf4b73d269e4d78ac1fd7d
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-28 11:20:38 +0200

    Отримує значення з GET-запиту, фільтрує та повертає очищений рядок.

--- bootstrap/classes/direct.class.php
+++ bootstrap/classes/direct.class.php
@@ -7,4 +7,19 @@
 namespace livecms;
 
 class direct {
+
+    /**
+     * Отримує значення з GET-запиту, фільтрує та повертає очищений рядок.
+     *
+     * @param string $get_name Назва параметра в запиті
+     * @return string Очищене значення параметра або 'no_data', якщо значення відсутнє
+     */
+
+    public static function get(string $get_name): string
+    {
+        $filter = filter_input(INPUT_GET, $get_name, FILTER_SANITIZE_ENCODED) ?? '';
+        $get = clearSpecialChars($filter);
+        return getStringLength($get) > 0 ? $get : 'no_data';
+    }
+
 }
