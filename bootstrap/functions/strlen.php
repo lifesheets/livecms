ed373569afa7e7801a6d31c72e3c1f29d7eadc29
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-28 11:17:48 +0200

    Функція підрахунку довжини рядка без переносів рядка

---
+++ bootstrap/functions/strlen.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Функція підрахунку довжини рядка без переносів рядка
+ *
+ * @param string|null $text Вхідний рядок
+ * @return int Довжина рядка без символів нового рядка
+ */
+
+function getStringLength(?string $text): int
+{
+    // Якщо $text дорівнює null, повертаємо 0, Видаляємо всі символи переносу рядка
+    return $text === null ? 0 : mb_strlen(str_replace(["\r", "\n"], '', $text), 'UTF-8');
+}
