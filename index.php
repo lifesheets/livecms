6812dc9374c3c9fc786236bece5c09681fba8be3
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-28 11:12:00 +0200

    Підключаємо файли системного ядра

--- index.php
+++ index.php
@@ -6,3 +6,6 @@ declare(strict_types=1);
 # Визначаємо кореневий каталог сайту
 const ROOT_DIR = __DIR__;
 
+# Підключаємо файли системного ядра
+require ROOT_DIR . '/bootstrap/engine.php';
+require ROOT_DIR . '/bootstrap/router.php';
