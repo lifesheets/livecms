019b43ec0bff339b9094b64d406aad063d5e5001
ua.lifesheets <ua.lifesheets@gmail.com>
2025-03-06 07:34:39 +0200

    jQuery v3.7.1

--- styling/backend/web/footer.php
+++ styling/backend/web/footer.php
@@ -1,4 +1,6 @@
 // Todo
+        <!-- Mainly Plugin Scripts -->
+        <script src="/nucleus/libraries/jquery/v3.7.1/js/jquery.min.js""></script>
         <script src="/nucleus/libraries/bootstrap/v5.3.3/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
